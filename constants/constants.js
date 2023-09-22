849cbde880cd4d97fc53ed55b4e0e1f995005912
sophe4rum <sohearum14@gmail.com>
2023-09-22 17:40:19 +0700

    first

---
+++ constants/constants.js
@@ -0,0 +1,68 @@
+export const NAME = 'Sorn Sophearum';
+
+export const DESIGNATION = `Fullstack developer | Data Analyst | UX/UI Designer`;
+
+export const DETAILS = {
+  Residence: 'Phnom Penh',
+  City: 'Phnom Penh',
+  Age: '23',
+};
+
+export const SKILLS = [
+  {
+    title: ' Full stack Developer',
+    level: '85%',
+  },
+  {
+    title: 'Next JS Developer',
+    level: '90%',
+  },
+  {
+    title: 'Data Analyst',
+    level: '75%',
+  },
+  {
+    title: 'UX/UI Designer',
+    level: '55%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '80%',
+  },
+];
+
+export const TECH_STACK = [
+  'ReactJS',
+  'JavaScript',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'ElectronJS',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Styled Components',
+  'Git',
+  'GitHub',
+  'BitBucket',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'sophearum14@gmail.com',
+  PHONE: '+85570233474',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/sophe4rum',
+  LINKEDIN: 'https://www.linkedin.com/in/sorn-sophearum-6a5755271/',
+  TWITTER: 'https://twitter.com/rum_sorn',
+  FACEBOOK: 'https://www.facebook.com/sophearum.sorn',
+};
---
+++ pages/api/recommendations.js
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const recommendationCard = [
+  {
+    id: 0,
+    name: 'Seangleng',
+    image: "images/leng.jpg",
+    designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
+    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
+    linkednURL: "#"
+  },
+  {
+    id: 1,
+    name: 'Sun Jessica',
+    image: "images/jessi.jpg",
+    designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
+    view: "I wholeheartedly recommend Osama as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Osama's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
+    linkednURL: "#"
+  },
+  {
+    id: 2,
+    name: 'Navatt Takon',
+    image: "images/somboleap.jpg",
+    designation: 'MERN Stack Developer at Productbox',
+    view: "I highly recommend Osama Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
+    linkednURL: "#"
+  },
+  {
+    id: 3,
+    name: 'Phon Sobon',
+    image: "images/sobon.jpg",
+    designation: 'GSR MERL | Grad student at UC Santa Cruz',
+    view: "Osama is a great student who is an enthusiastic learner. He is always ready to learn new skills and is a very hard worker. His leadership skills are maturing and he is in the right direction for success.",
+    linkednURL: "#"
+  },
+]
+export default function handler(req, res) {
+  res.status(200).json(recommendationCard)
+}
