e86d592cb91912569121c64a15191b15fd48ced5
sirblue <sophearum14@gmail.com>
2023-11-15 16:24:33 +0700

    changed footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                     <span>2023 All Rights Reserved.</span>
                 </div>
                 <div className="flex items-center">
-                    <a href='https://www.fiverr.com/codeworthy' target="_blank" rel="noreferrer">Made with ❤️ by <span className='font-bold'>Sophearum</span></a>
+                    <a href='https://github.com/sirbluee' target="_blank" rel="noreferrer">Made by <span className='font-bold'>Sophearum ❤️</span></a>
                 </div>
                 <div className="hidden md:flex items-center">
                     <div className="mr-1 text-base">
