22e30c1e9411eeb796c4bfd8c2fb1bc6f0dbac2b
sirblue <sophearum14@gmail.com>
2023-11-15 14:00:43 +0700

    update cv

--- components/Common/Intro/Download.jsx
+++ components/Common/Intro/Download.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 const Download = ({ icon }) => {
   return (
     <>
-      <Link href='/osamajavaid-resume.pdf' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
-        <span className='text-Snow'>Download Resume</span>
+      <Link href='/my-cv.pdf' target='_blank' className='flex flex-row text-LightGray items-center gap-x-4 pb-14 pt-4'>
+        <span className='text-Snow target-blank'>Download Resume</span>
         <span>{icon}</span>
       </Link>
     </>
