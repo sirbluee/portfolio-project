a5f535a5127c466148a7670065f99d11f2020dd1
sirblue <sophearum14@gmail.com>
2024-08-15 16:19:51 +0700

    added seo

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Sorn Sophearum | Home</title>
+        <meta name="description" content="Welcome to my portfolio website. Explore my work and projects." />
+        <meta name="keywords" content="portfolio, web developer, projects" />
+        <meta name="author" content="Sorn Sophearum" />
+        <meta property="og:title" content="Sorn Sophearum | Home" />
+        <meta property="og:description" content="Welcome to my portfolio website. Explore my work and projects." />
+        <meta property="og:image" content="https://surveybox.istad.co/_next/image?url=%2Fimages%2Fabout%2Fmember%2Fphearum.png&w=256&q=75" />
+        <meta property="og:url" content="https://sorn-sophearum.vercel.app" />
+        <meta name="twitter:card" content="https://surveybox.istad.co/_next/image?url=%2Fimages%2Fabout%2Fmember%2Fphearum.png&w=256&q=75" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ sitemap.ts
@@ -0,0 +1,22 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://sorn-sophearum.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://sorn-sophearum.vercel.app/background',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sorn-sophearum.vercel.app/contact',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}
