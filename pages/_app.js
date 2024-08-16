61d19b56de88760f4e989a6e50df4f4eee02c1e5
sirblue <sophearum14@gmail.com>
2024-08-16 15:12:28 +0700

    remove speed vercel

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Layout from './layout'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
@@ -10,7 +9,6 @@ export default function App({ Component, pageProps }) {
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
-        <SpeedInsights />
         <Analytics />
       </Layout>
     </QueryClientProvider>
