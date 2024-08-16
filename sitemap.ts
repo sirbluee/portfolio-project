5894dc41214dc4e8de78b733851e56e6e6ef235f
sirblue <sophearum14@gmail.com>
2024-08-16 11:55:07 +0700

    modified sitemap

--- sitemap.ts
+++ sitemap.ts
@@ -1,22 +1,22 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://sorn-sophearum.vercel.app',
+      url: "https://sorn-sophearum.vercel.app",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: 'https://sorn-sophearum.vercel.app/background',
+      url: "https://sorn-sophearum.vercel.app/background",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: 'https://sorn-sophearum.vercel.app/contact',
+      url: "https://sorn-sophearum.vercel.app/contact",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.5,
     },
-  ]
+  ];
 }
