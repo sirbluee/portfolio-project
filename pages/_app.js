2bc1acc49ed34957395fe0d0b9b298af891fa2d9
sirblue <sophearum14@gmail.com>
2024-08-16 15:06:43 +0700

    added speed vercel

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import Layout from './layout'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }) {
     <QueryClientProvider client={queryClient}>
       <Layout>
         <Component {...pageProps} />
+        <SpeedInsights />
         <Analytics />
       </Layout>
     </QueryClientProvider>
